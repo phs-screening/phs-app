965e01dc05e27a7ee99ffba2e226ad8d3eef761e
winhuiwy <winhuiwy@users.noreply.github.com>
2021-03-28 01:49:48 +0800

    login and form basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
 
+import Login from "./Login";
+import HomePage from "./HomePage";
+import Form1 from "./Form1";
+
 function App() {
+  var authUser = null;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!authUser ? (
+        <Login />
+      ) : (
+        <div className="App_body">
+          <HomePage />
+        </div>
+      )}
     </div>
   );
 }
