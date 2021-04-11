d5b48332dd29cbac1b1300d337ee0d6917e42c3d
winhuiwy <winhuiwy@users.noreply.github.com>
2021-04-11 17:24:49 +0800

    added router and pre-reg form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
 
 import Login from "./Login";
 import HomePage from "./HomePage";
 import Form1 from "./Form1";
+import Form2 from "./Form2";
 
 function App() {
-  var authUser = null;
+  var authUser = true;
   return (
     <div className="App">
       {!authUser ? (
         <Login />
       ) : (
         <div className="App_body">
-          <HomePage />
+          <Router>
+
+          
+
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              
+              <Route exact path="/form1" component={Form1} />
+
+
+              <Route exact path="/form2">
+                <Form2 />
+              </Route>
+
+            </Switch>         
+          
+          </Router>
+
         </div>
       )}
     </div>
