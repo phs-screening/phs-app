de3a543e76f5462f7d909a2bc6d501ab4af1f0f0
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2021-07-25 15:10:37 +0800

    update prereg form field names

--- src/forms/prereg.js
+++ src/forms/prereg.js
@@ -6,13 +6,13 @@ import { RadioField, LongTextField } from 'uniforms-material';
 
 
 export const schema = new SimpleSchema({
-        preRegistrationQ1: {
+        gender: {
           type: String, allowedValues: ["Male", "Female"], optional: false
-        }, preRegistrationQ2: {
+        }, initials: {
           type: String, optional: false
-        }, preRegistrationQ3: {
+        }, abbreviatedNric: {
           type: String, optional: false, regEx: /^[0-9]{3}[a-zA-Z]$/
-        }, preRegistrationQ4: {
+        }, goingForPhlebotomy: {
           type: String, allowedValues: ["Y", "N"], optional: false
         }
     }
@@ -22,13 +22,13 @@ export const layout = (
     <Fragment>
     <h2>Pre-Registration</h2>
     Gender
-    <RadioField name="preRegistrationQ1" />
+    <RadioField name="gender" />
     Initials (Surname must be spelt out. E.g. John Tan Soo Keng = Tan S.K.J. ; Alan Simon Lee = A.S. Lee)
-    <LongTextField name="preRegistrationQ2" />
+    <LongTextField name="initials" />
     Last 4 characters of NRIC (e.g. 987A)
-    <LongTextField name="preRegistrationQ3" />
+    <LongTextField name="abbreviatedNric" />
     Going for Phlebotomy?<br /><br /><i>Conditions:<br />1) Fasted for minimum 8 hours <br />          Note: Water is allowed, coffee/tea is not. Medications are fine. <br />2) NOT previously diagnosed with Diabetes/ High Cholesterol/ High Blood Pressure.<br />3) Have not done a blood test within 1 year.</i>
-    <RadioField name="preRegistrationQ4" />
+    <RadioField name="goingForPhlebotomy" />
     
   </Fragment>
   )
