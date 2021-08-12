ed9f8ea60302acb7b5f01a9f58467d15e7876856
Winnie <winniehui.99@gmail.com>
2021-08-12 22:04:08 +0800

    combined geriptconsult, gerisppb, geritug, gerivision

--- src/forms/GeriPtConsultForm.js
+++ src/forms/GeriPtConsultForm.js
@@ -1,47 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
 
 import Divider from '@material-ui/core/Divider';
 import Paper from '@material-ui/core/Paper';
 
 import { AutoForm } from 'uniforms';
 import { SubmitField, ErrorsField } from 'uniforms-material';
+import { RadioField, LongTextField } from 'uniforms-material';
 
-import { schema, layout } from './geriPtConsult';
+const schema = new SimpleSchema({
+  geriPtConsultQ1: {
+    type: String, optional: false
+  }, geriPtConsultQ2: {
+    type: String, allowedValues: ["Yes", "No"], optional: false
+  }, geriPtConsultQ3: {
+    type: String, optional: true
+  }, geriPtConsultQ4: {
+    type: String, allowedValues: ["Yes", "No"], optional: false
+  }, geriPtConsultQ5: {
+    type: String, optional: true
+  }
+}
+)
 
 class GeriPtConsultForm extends Component {
 
-    render() {
-        const form_schema = new SimpleSchema2Bridge(schema)
-        const form_layout = layout
-        const newForm = () => (
-          <AutoForm
-            schema={form_schema}
-            onSubmit={console.log}
-            //onSubmit={this.handleSubmit}
-            onSubmitSuccess={() => {
-                alert("Successful");
-              }}
-            onSubmitFailure={() => {
-                alert('Unsuccessful')
-              }}
-          >
-            {form_layout}
-            <ErrorsField />
-            <div>
-              <SubmitField inputRef={(ref) => this.formRef = ref} />
-            </div>
-            
-            <br /><Divider />
-          </AutoForm>
-        );
-        
-        return (
-          <Paper elevation={2} p={0} m={0}>
-            {newForm()}
-          </Paper>
-        );
-      }
+  render() {
+    const form_schema = new SimpleSchema2Bridge(schema)
+    const newForm = () => (
+      <AutoForm
+        schema={form_schema}
+        onSubmit={console.log}
+        //onSubmit={this.handleSubmit}
+        onSubmitSuccess={() => {
+          alert("Successful");
+        }}
+        onSubmitFailure={() => {
+          alert('Unsuccessful')
+        }}
+      >
+        <Fragment>
+          <h2>3.4a PT Consult</h2>
+          Memo:
+          <LongTextField name="geriPtConsultQ1" label="Geri - PT Consult Q1" />
+          To be referred for doctor's consult (PT)?
+          <RadioField name="geriPtConsultQ2" label="Geri - PT Consult Q2" />
+          Reasons for referral to Doctor's consult (PT):
+          <LongTextField name="geriPtConsultQ3" label="Geri - PT Consult Q3" />
+          To be referred for social support (For HDB EASE Sign-up) (PT):
+          <RadioField name="geriPtConsultQ4" label="Geri - PT Consult Q4" />
+          Reasons for referral to social support (PT):
+          <LongTextField name="geriPtConsultQ5" label="Geri - PT Consult Q5" />
+          <h2>IF THE PATIENT NEEDS TO GO TO DOCTOR'S CONSULT/ SOCIAL SUPPORT MODALITY THAT YOU RECOMMENDED, PLEASE EDIT ON FORM A</h2>
+
+        </Fragment>
+        <ErrorsField />
+        <div>
+          <SubmitField inputRef={(ref) => this.formRef = ref} />
+        </div>
+
+        <br /><Divider />
+      </AutoForm>
+    );
+
+    return (
+      <Paper elevation={2} p={0} m={0}>
+        {newForm()}
+      </Paper>
+    );
+  }
 }
 
 export default GeriPtConsultForm;
--- src/forms/GeriTugForm.js
+++ src/forms/GeriTugForm.js
@@ -1,47 +1,77 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
 
 import Divider from '@material-ui/core/Divider';
 import Paper from '@material-ui/core/Paper';
 
 import { AutoForm } from 'uniforms';
 import { SubmitField, ErrorsField } from 'uniforms-material';
+import { TextField, SelectField, RadioField } from 'uniforms-material';
 
-import { schema, layout } from './geriTug';
+const schema = new SimpleSchema({
+  geriTugQ1: {
+    type: Array, optional: true
+  }, "geriTugQ1.$": {
+    type: String, allowedValues: ["Walking frame", "Walking frame with wheels", "Crutches/ Elbow crutches", "Quadstick (Narrow/ Broad)", "Walking stick", "Umbrella", "Others (Please specify in textbox )"]
+  }, geriTugQ2: {
+    type: String, optional: true
+  }, geriTugQ3: {
+    type: String, optional: false
+  }, geriTugQ4: {
+    type: String, allowedValues: ["High Falls Risk (> 15sec)", "Low Falls Risk (≤ 15 sec)"], optional: false
+  }, geriTugQ5: {
+    type: String, allowedValues: ["Yes", "No"], optional: false
+  }
+}
+)
 
 class GeriTugForm extends Component {
 
-    render() {
-        const form_schema = new SimpleSchema2Bridge(schema)
-        const form_layout = layout
-        const newForm = () => (
-          <AutoForm
-            schema={form_schema}
-            onSubmit={console.log}
-            //onSubmit={this.handleSubmit}
-            onSubmitSuccess={() => {
-                alert("Successful");
-              }}
-            onSubmitFailure={() => {
-                alert('Unsuccessful')
-              }}
-          >
-            {form_layout}
-            <ErrorsField />
-            <div>
-              <SubmitField inputRef={(ref) => this.formRef = ref} />
-            </div>
-            
-            <br /><Divider />
-          </AutoForm>
-        );
-        
-        return (
-          <Paper elevation={2} p={0} m={0}>
-            {newForm()}
-          </Paper>
-        );
-      }
+  render() {
+    const form_schema = new SimpleSchema2Bridge(schema)
+    const newForm = () => (
+      <AutoForm
+        schema={form_schema}
+        onSubmit={console.log}
+        //onSubmit={this.handleSubmit}
+        onSubmitSuccess={() => {
+          alert("Successful");
+        }}
+        onSubmitFailure={() => {
+          alert('Unsuccessful')
+        }}
+      >
+        <Fragment>
+          <h2>3.3b Time-Up and Go (TUG)</h2>
+          Walking aid (if any):
+          <SelectField name="geriTugQ1" checkboxes="true" label="Geri - TUG Q1" />
+          Please Specify for Others
+          <TextField name="geriTugQ2" label="Geri - TUG Q2" />
+          Time taken (in seconds):
+          <TextField name="geriTugQ3" label="Geri - TUG Q3" />
+          <h3>If > 15 seconds, participant has a high falls risk.</h3>
+          Falls Risk Level:
+          <RadioField name="geriTugQ4" label="Geri - TUG Q4" />
+          *Referral to Physiotherapist and Occupational Therapist Consult
+          <RadioField name="geriTugQ5" label="Geri - TUG Q5" />
+
+        </Fragment>
+        <ErrorsField />
+        <div>
+          <SubmitField inputRef={(ref) => this.formRef = ref} />
+        </div>
+
+        <br /><Divider />
+      </AutoForm>
+    );
+
+    return (
+      <Paper elevation={2} p={0} m={0}>
+        {newForm()}
+      </Paper>
+    );
+  }
 }
 
 export default GeriTugForm;
