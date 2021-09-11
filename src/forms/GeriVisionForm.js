2c365a7328ddea66b902755b857858a2ab3c151b
Winnie <winniehui.99@gmail.com>
2021-09-11 18:53:56 +0800

    geri vision done

--- src/forms/GeriVisionForm.js
+++ src/forms/GeriVisionForm.js
@@ -7,7 +7,8 @@ import Paper from '@material-ui/core/Paper';
 
 import { AutoForm } from 'uniforms';
 import { SubmitField, ErrorsField } from 'uniforms-material';
-import { TextField, SelectField, RadioField } from 'uniforms-material';
+import { LongTextField, SelectField, RadioField, NumField } from 'uniforms-material';
+import PopupText from 'src/utils/popupText';
 
 const schema = new SimpleSchema({
   geriVisionQ1: {
@@ -31,7 +32,7 @@ const schema = new SimpleSchema({
   }, geriVisionQ9: {
     type: Array, optional: true
   }, "geriVisionQ9.$": {
-    type: String, allowedValues: ["Referred to Doctor\s Consult"]
+    type: String, allowedValues: ["Referred to Doctor's Consult"]
   }
 }
 )
@@ -56,24 +57,29 @@ class GeriVisionForm extends Component {
           <h2>2. VISION SCREENING</h2>
           1. Previous eye condition or surgery
           <RadioField name="geriVisionQ1" label="Geri - Vision Q1" />
-          Explanation
-          <TextField name="geriVisionQ2" label="Geri - Vision Q2" />
-          2. Visual acuity (w/o pinhole occluder) - Right Eye 6/__
-          <TextField name="geriVisionQ3" label="Geri - Vision Q3" />
-          3. Visual acuity (w/o pinhole occluder) - Left Eye 6/__
-          <TextField name="geriVisionQ4" label="Geri - Vision Q4" />
-          4. Visual acuity (with pinhole) *only if VA w/o pinhole is ≥ 6/12 - Right Eye 6/__
-          <TextField name="geriVisionQ5" label="Geri - Vision Q5" />
-          5. Visual acuity (with pinhole) *only if VA w/o pinhole is ≥ 6/12 - Left Eye 6/__
-          <TextField name="geriVisionQ6" label="Geri - Vision Q6" />
+          <PopupText qnNo="geriVisionQ1" triggerValue="Yes (Specify in textbox )">
+            <Fragment>
+              Explanation
+              <LongTextField name="geriVisionQ2" label="Geri - Vision Q2" />
+            </Fragment>
+          </PopupText>
+          
+          2. Visual acuity (w/o pinhole occluder) - Right Eye 6/__ <br />
+          <NumField name="geriVisionQ3" label="Geri - Vision Q3" /> <br />
+          3. Visual acuity (w/o pinhole occluder) - Left Eye 6/__ <br />
+          <NumField name="geriVisionQ4" label="Geri - Vision Q4" /> <br />
+          4. Visual acuity (with pinhole) *only if VA w/o pinhole is ≥ 6/12 - Right Eye 6/__ <br />
+          <NumField name="geriVisionQ5" label="Geri - Vision Q5" /> <br />
+          5. Visual acuity (with pinhole) *only if VA w/o pinhole is ≥ 6/12 - Left Eye 6/__ <br />
+          <NumField name="geriVisionQ6" label="Geri - Vision Q6" /> <br />
           6. Eye Functional Test *only applicable if vision is worse than 6/60
           <RadioField name="geriVisionQ7" label="Geri - Vision Q7" />
-          Please refer to Occupational Therapist Consult if visual acuity is ≥ 6/12
-          <SelectField name="geriVisionQ8" checkboxes="true" label="Geri - Vision Q8" />
-          Please refer to Doctor’s Consult if pinhole visual acuity is ≥ 6/12
-          <SelectField name="geriVisionQ9" checkboxes="true" label="Geri - Vision Q9" />
-          <h2>IF THE PATIENT NEEDS TO GO TO DOCTOR'S CONSULT MODALITY THAT YOU RECOMMENDED, PLEASE EDIT ON THE MSS TAB UNDER 'REGISTRATION'.</h2>
-
+          <br />Please <b>refer to Occupational Therapist Consult</b> if visual acuity is <b>≥ 6/12</b>
+          <br /><SelectField name="geriVisionQ8" checkboxes="true" label="Geri - Vision Q8" />
+          <br />Please <b>refer to L2 Eye Screening (Eye Bus/ NUHS)</b> if pinhole visual acuity <u><b>is > 6/12</b></u><br />
+          If participant is required to go for L2 Eye Screening, encourage participant to go to Eye Bus/ NUHS after Screening Review.
+          <br /><RadioField name="geriVisionQ9" label="Geri - Vision Q9" />
+          
         </Fragment>
         <ErrorsField />
         <div>
