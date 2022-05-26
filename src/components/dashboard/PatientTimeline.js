53a083741a1795834b63cc577cd3ead44bb3581f
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2022-05-26 22:54:48 +0800

    Improve rendering of timeline

--- src/components/dashboard/PatientTimeline.js
+++ src/components/dashboard/PatientTimeline.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Timeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
-
+import { useNavigate } from 'react-router-dom';
+import mongoDB from "../../services/mongoDB"
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {
   Box,
   Button,
@@ -15,90 +17,185 @@ import {
   Divider,
 } from '@material-ui/core';
 
-var formDone = [
-  true,
-  true,
-  true,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false
-];
 
-const BasicTimeline = (props) => (
+function generateStatusArray(record) {
+  const entries = 10;
+  if (record) {
+    return [
+      true, // pre-registration, always true 
+      record.registrationForm !== undefined, // registration
+      record.phlebotomyForm !== undefined, // phlebotomy
+      record.historyTakingForm !== undefined, // history taking
+      record.fitForm !== undefined, // fit
+      record.wceForm !== undefined, // wce
+      record.geriatricsForm !== undefined, // geriatrics
+      record.doctorConsultForm !== undefined, // doctor's consult
+      record.socialServiceForm !== undefined, // social service
+      record.feedbackForm !== undefined // feedback
+    ]
+  } else {
+    return new Array(entries).fill(false);;
+  }
+}
+
+function navigateTo(event, navigate, page) {
+  event.preventDefault();
+  const path = "/app/" + page;
+  navigate(path, { replace: true });
+}
+
+const BasicTimeline = (props) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [goingForPhlebotomy, setGoingForPhlebotomy] = useState();
+  const [formDone, setFormDone] = useState([]);
+
+  useEffect(() => {
+    const createFormsStatus = async () => {
+      try {
+        const mongoConnection = mongoDB.currentUser.mongoClient("mongodb-atlas");
+        const patientsRecord = mongoConnection.db("phs").collection("patients");
+        // patientId must be valid for this component to even render without error
+        // hence, if there is no record, likely there is implementation bug
+        const record = await patientsRecord.findOne({queueNo: props.patientId});
+        setGoingForPhlebotomy(record.goingForPhlebotomy === "Y");
+        setFormDone(generateStatusArray(record));
+        setLoading(false);
+      } catch(err) {
+        alert(err);
+        navigate('/app/registration', { replace: true });
+      }
+    };
+    createFormsStatus();
+  }, [navigate, props.patientId]);
+if (loading) {
+  return (<div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}}><CircularProgress /></div>);
+} else {
+  return (
   <Timeline>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[0] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[0] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
       <TimelineContent><a href="/app/prereg">Pre-Registration</a></TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[1] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[1] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/reg">Registration</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/reg"
+          onClick={(event) => navigateTo(event, navigate, "reg")}>Registration
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[2] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[2]
+          ? <TimelineDot color="primary"/>
+          : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/phlebo">Phlebotomy</a></TimelineContent>
+      <TimelineContent>
+      {goingForPhlebotomy
+          ? <a
+              href="/app/phlebo"
+              onClick={(event) => navigateTo(event, navigate, "phlebo")}>Phlebotomy
+            </a>
+          : <p>[Not required] Phlebotomy</p>
+      }
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[3] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[3] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/hxtaking">History Taking</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/hxtaking"
+          onClick={(event) => navigateTo(event, navigate, "hxtaking")}>History Taking
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[4] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[4] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/fit">FIT</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/fit"
+          onClick={(event) => navigateTo(event, navigate, "fit")}>FIT
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[5] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[5] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/wce">WCE</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/wce"
+          onClick={(event) => navigateTo(event, navigate, "wce")}>WCE
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[6] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[6] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/geri">Geriatrics</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/geri"
+          onClick={(event) => navigateTo(event, navigate, "geri")}>Geriatrics
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[7] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[7] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/doctorsconsult">Doctor's Consult</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/doctorsconsult"
+          onClick={(event) => navigateTo(event, navigate, "doctorsconsult")}>Doctor's Consult
+          </a>
+        </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[8] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[8] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/socialservice">Social Service</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/socialservice"
+          onClick={(event) => navigateTo(event, navigate, "socialservice")}>Social Service
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
-        {props.formDone[9] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
+        {formDone[9] ? <TimelineDot color="primary"/> : <TimelineDot color="grey"/>}
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent><a href="/app/feedback">Feedback</a></TimelineContent>
+      <TimelineContent>
+        <a
+          href="/app/feedback"
+          onClick={(event) => navigateTo(event, navigate, "feedback")}>Feedback
+        </a>
+      </TimelineContent>
     </TimelineItem>
     <TimelineItem>
       <TimelineSeparator>
@@ -107,7 +204,7 @@ const BasicTimeline = (props) => (
       <TimelineContent>END</TimelineContent>
     </TimelineItem>
   </Timeline>
-);
+)}};
 
 const PatientTimeline = (props) => (
   <Card {...props}>
@@ -131,7 +228,7 @@ const PatientTimeline = (props) => (
           position: "relative"
         }}
       >
-        <BasicTimeline formDone={props.formDone}/>
+        <BasicTimeline patientId={props.patientId}/>
       </Box>
     </CardContent>
     <Divider />
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import mongoDB from "../services/mongoDB";
 import {
   Box,
   Container,
@@ -10,55 +9,16 @@ import {Helmet} from "react-helmet";
 import { FormContext } from 'src/api/utils';
 import { useNavigate } from 'react-router-dom';
 
-function generateStatusArray(record) {
-  const entries = 10;
-  if (record) {
-    return [
-      true, // pre-registration, always true 
-      record.registrationForm !== undefined, // registration
-      record.phlebotomyForm !== undefined, // phlebotomy
-      record.historyTakingForm !== undefined, // history taking
-      record.fitForm !== undefined, // fit
-      record.wceForm !== undefined, // wce
-      record.geriatricsForm !== undefined, // geriatrics
-      record.doctorConsultForm !== undefined, // doctor's consult
-      record.socialServiceForm !== undefined, // social service
-      record.feedbackForm !== undefined // feedback
-    ]
-  } else {
-    return new Array(entries).fill(false);;
-  }
-}
-
-
 const Dashboard = () =>{
   const {patientId, updatePatientId} = useContext(FormContext);
-  const [formsStatus, setFormsStatus] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (patientId === -1) {
         alert("You need to enter the queue number for the patient you are attending to!")
         navigate('/app/registration', { replace: true });
-    } else {
-      // TODO: query patients collection and pass it as props to child component
-      const createFormsStatus = async () => {
-        try {
-          const mongoConnection = mongoDB.currentUser.mongoClient("mongodb-atlas");
-          const patientsRecord = mongoConnection.db("phs").collection("patients");
-          // patientId must be valid for this component to even render without error
-          // hence, if there is no record, likely there is implementation bug
-          const record = await patientsRecord.findOne({queueNo: patientId});
-          setFormsStatus(generateStatusArray(record));
-        } catch(err) {
-          alert(err);
-          navigate('/app/registration', { replace: true });
-        }
-        
-      };
-      createFormsStatus();
     }
-}, [patientId, navigate])
+}, [navigate, patientId])
 
     return (
 
@@ -85,7 +45,7 @@ const Dashboard = () =>{
             xl={9}
             xs={12}
           >
-            <PatientTimeline formDone={formsStatus}/>
+            <PatientTimeline patientId={patientId} />
           </Grid>
         </Grid>
       </Container>
