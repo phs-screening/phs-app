14c62f764dd2a33f090aaec60a16c478f67086e8
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2022-05-29 23:37:03 +0800

    Fix the issue where the scrollbar is not at top after navigating pages

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,4 +1,5 @@
 import React from 'react';
 
 // helper & utility functions, especially those that are common to multiple APIs.
-export const FormContext = React.createContext()
+export const FormContext = React.createContext();
+export const ScrollTopContext = React.createContext();
