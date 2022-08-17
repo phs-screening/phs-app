0fdec4a9769037831bccfcb65517b284fad1a6d3
ryantianj <ryantianjun@gmail.com>
2022-08-17 13:40:45 +0800

    Add geriAppt side panel

--- src/forms/GeriGeriApptForm.js
+++ src/forms/GeriGeriApptForm.js
@@ -11,10 +11,13 @@ import { AutoForm } from 'uniforms';
 import { SubmitField, ErrorsField } from 'uniforms-material';
 import { SelectField, RadioField, BoolField } from 'uniforms-material';
 import PopupText from 'src/utils/popupText';
-import { submitForm } from '../api/api.js';
+import {calculateBMI, calculateSppbScore, submitForm} from '../api/api.js';
 import { FormContext } from '../api/utils.js';
 import {getSavedData} from "../services/mongoDB";
 import './fieldPadding.css'
+import Grid from "@material-ui/core/Grid";
+import {blueText, title, underlined} from "../theme/commonComponents";
+import allForms from "./forms.json";
 
 const schema = new SimpleSchema({
   // geriGeriApptQ1: {
@@ -60,9 +63,29 @@ const GeriGeriApptForm = (props) => {
     const [form_schema, setForm_schema] = useState(new SimpleSchema2Bridge(schema))
     const navigate = useNavigate();
     const [saveData, setSaveData] = useState({})
+    const [loadingSidePanel, isLoadingSidePanel] = useState(true);
+    const [geriVision, setGeriVision] = useState({})
+    const [geriEbas, setGeriEbas] = useState({})
+    const [geriPt, setGeriPt] = useState({})
+    const [geriOt, setGeriOt] = useState({})
+    const [geriPhysical, setGeriPhysical] = useState({})
     useEffect(async () => {
-        const savedData = await getSavedData(patientId, formName);
-        setSaveData(savedData)
+        const savedData = getSavedData(patientId, formName);
+        const geriVisionData = getSavedData(patientId, allForms.geriVisionForm)
+        const geriEbasData = getSavedData(patientId, allForms.geriEbasDepForm)
+        const geriPtData = getSavedData(patientId, allForms.geriPtConsultForm)
+        const geriOtData = getSavedData(patientId, allForms.geriOtConsultForm)
+        const geriPhysicalData = getSavedData(patientId, allForms.geriPhysicalActivityLevelForm)
+        Promise.all([savedData, geriVisionData, geriEbasData, geriPtData, geriOtData, geriPhysicalData]).then(result => {
+            setSaveData(result[0])
+            setGeriVision(result[1])
+            setGeriEbas(result[2])
+            setGeriPt(result[3])
+            setGeriOt(result[4])
+            setGeriPhysical(result[5])
+
+            isLoadingSidePanel(false)
+        })
     }, [])
     const newForm = () => (
       <AutoForm
@@ -181,9 +204,68 @@ const GeriGeriApptForm = (props) => {
     );
 
     return (
-      <Paper elevation={2} p={0} m={0}>
-        {newForm()}
-      </Paper>
+        <Paper elevation={2} p={0} m={0}>
+            <Grid display="flex" flexDirection="row" >
+                <Grid xs={9}>
+                    <Paper elevation={2} p={0} m={0}>
+                        {newForm()}
+                    </Paper>
+                </Grid>
+                <Grid
+                    p={1}
+                    width="30%"
+                    display="flex"
+                    flexDirection="column"
+                    alignItems={loadingSidePanel ? "center" : "left"}>
+                    {loadingSidePanel ? <CircularProgress />
+                        :
+                        <div>
+                            {title("SWCDC Eye Vouchers")}
+                            {underlined("Type of vision error:")}
+                            {geriVision ? blueText(geriVision.geriVisionQ8) : blueText("nil")}
+                            {title("Social Service")}
+                            {underlined("Referred to Social Service from Geri EBAS:")}
+                            {geriEbas ? blueText(geriEbas.geriEbasDepQ11) : blueText("nil")}
+                            {underlined("Reasons for referral:")}
+                            {geriEbas ? blueText(geriEbas.geriEbasDepQ12) : blueText("nil")}
+                            {underlined("Referred to Social Service (PT):")}
+                            {geriPt ? blueText(geriPt.geriPtConsultQ4) : blueText("nil")}
+                            {underlined("Reasons for referral:")}
+                            {geriPt ? blueText(geriPt.geriPtConsultQ5) : blueText("nil")}
+                            {underlined("Referred to Social Service (OT):")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ4) : blueText("nil")}
+                            {underlined("Reasons for referral:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ5) : blueText("nil")}
+                            {underlined("Programmes recommended by OT:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ6) : blueText("nil")}
+                            {underlined("Referred for HDB EASE Sign-Up:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ7) : blueText("nil")}
+                            {underlined("Reasons for referral:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ8) : blueText("nil")}
+                            {underlined("Indications for SWCDC S&S homes:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ9) : blueText("nil")}
+                            {underlined("Comments:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ10) : blueText("nil")}
+                            {title("Doctor's Consult")}
+                            {underlined("Reasons for referral to Doctor's Consult (PT):")}
+                            {geriPt ? blueText(geriPt.geriPtConsultQ3) : blueText("nil")}
+                            {underlined("Number of falls in past 1 year:")}
+                            {geriPhysical ? blueText(geriPhysical.geriPhysicalActivityLevelQ8) : blueText("nil")}
+                            {underlined("Were any of the falls injurious?")}
+                            {geriPhysical ? blueText(geriPhysical.geriPhysicalActivityLevelQ9) : blueText("nil")}
+                            {underlined("Referred to Doctor's Consult (OT):")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ2) : blueText("nil")}
+                            {underlined("Reasons for referral:")}
+                            {geriOt ? blueText(geriOt.geriOtConsultQ3) : blueText("nil")}
+                            {geriVision ? blueText(geriVision.geriVisionQ9) : blueText("nil")}
+
+
+
+                        </div>
+                    }
+                </Grid>
+            </Grid>
+        </Paper>
     );
 }
 
