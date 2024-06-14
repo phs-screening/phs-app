632f574269066f12c2987e7c682e869c03ea8964
rxchell <e0969575@u.nus.edu>
2024-06-14 22:05:58 +0800

    eligibility page routing

---
+++ src/pages/Eligibility.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { Box, Container, Grid } from '@mui/material'
+
+const Eligibility = () => (
+  <>
+    <Helmet>
+      <title>Patient Eligibility</title>
+    </Helmet>
+    <Box
+      sx={{
+        backgroundColor: 'background.default',
+        minHeight: '100%',
+        py: 3,
+      }}
+    >
+      <Container maxWidth='lg'>
+        <Grid container spacing={3}>
+          <Grid item lg={2} md={2} xs={0}></Grid>
+          <Grid item lg={8} md={8} xs={12}>
+          </Grid>
+          <Grid item lg={2} md={2} xs={0}></Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+)
+
+export default Eligibility
