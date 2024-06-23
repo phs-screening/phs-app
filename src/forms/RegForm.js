6ca75d1db1ff5c9c9f4df66574f63683a2de8ecf
ylyma <amylingyuhui@gmail.com>
2024-06-24 01:26:56 +0800

    update regform

--- src/forms/RegForm.js
+++ src/forms/RegForm.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useEffect, useState } from 'react'
+import React, { createRef, Fragment, useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2'
 import SimpleSchema from 'simpl-schema'
@@ -15,6 +15,8 @@ import {
   LongTextField,
   TextField,
   BoolField,
+  DateField,
+  NumField,
 } from 'uniforms-material'
 import CircularProgress from '@mui/material/CircularProgress'
 import { submitForm, submitRegClinics } from '../api/api.js'
@@ -22,6 +24,7 @@ import { FormContext } from '../api/utils.js'
 import { getClinicSlotsCollection, getSavedData } from '../services/mongoDB'
 import './fieldPadding.css'
 import './forms.css'
+import { useField } from 'uniforms'
 
 const postalCodeToLocations = {
   600415: 'Pandan Clinic\nBIk 415, Pandan Gardens #01- 115, S600415',
@@ -70,6 +73,9 @@ const RegForm = () => {
         temp[postalCode] -= counterItems.length
       }
     }
+    if (!savedData.registrationQ4) {
+      savedData.registrationQ4 = new Date()
+    }
     setSlots(temp)
 
     setSaveData(savedData)
@@ -91,6 +97,20 @@ const RegForm = () => {
     })
     return result
   }
+
+  const GetAge = () => {
+    const [{ value: birthday }] = useField('registrationQ4', {})
+    const today = new Date()
+    if (birthday) {
+      var age = today.getFullYear() - birthday.getFullYear()
+      const monthDiff = today.getMonth() - birthday.getMonth()
+      if (monthDiff < 0 || (monthDiff == 0 && today.getDate() < birthday.getDate())) {
+        age--
+      }
+      return <p className='blue'>{age}</p>
+    }
+    return null
+  }
   const formOptions = {
     registrationQ1: [
       { label: 'Mr', value: 'Mr' },
@@ -98,21 +118,38 @@ const RegForm = () => {
       { label: 'Mrs', value: 'Mrs' },
       { label: 'Dr', value: 'Dr' },
     ],
-    registrationQ3: [
+    registrationQ6: [
+      {
+        label: 'Male',
+        value: 'Male',
+      },
+      { label: 'Female', value: 'Female' },
+    ],
+    registrationQ7: [
+      {
+        label: 'Chinese 华裔',
+        value: 'Chinese 华裔',
+      },
+      { label: 'Malay 巫裔', value: 'Malay 巫裔' },
+      { label: 'Indian 印裔', value: 'Indian 印裔' },
+      { label: 'Eurasian 欧亚裔', value: 'Eurasian 欧亚裔' },
+      { label: 'Others 其他', value: 'Others 其他' },
+    ],
+    registrationQ8: [
       { label: 'Singapore Citizen 新加坡公民', value: 'Singapore Citizen 新加坡公民' },
       {
         label: 'Singapore Permanent Resident (PR) \n新加坡永久居民',
         value: 'Singapore Permanent Resident (PR) \n新加坡永久居民',
       },
     ],
-    registrationQ4: [
+    registrationQ9: [
       { label: 'Single 单身', value: 'Single 单身' },
       { label: 'Married 已婚', value: 'Married 已婚' },
       { label: 'Widowed 已寡', value: 'Widowed 已寡' },
       { label: 'Separated 已分居', value: 'Separated 已分居' },
       { label: 'Divorced 已离婚', value: 'Divorced 已离婚' },
     ],
-    registrationQ6: [
+    registrationQ11: [
       { label: 'Jurong', value: 'Jurong' },
       { label: 'Yuhua', value: 'Yuhua' },
       { label: 'Bukit Batok', value: 'Bukit Batok' },
@@ -121,18 +158,26 @@ const RegForm = () => {
       { label: 'Hong Kah North', value: 'Hong Kah North' },
       { label: 'Others', value: 'Others' },
     ],
-    registrationQ8: [
+    registrationQ12: [
+      {
+        label: 'Yes',
+        value: 'Yes',
+      },
+      { label: 'No', value: 'No' },
+      { label: 'Unsure', value: 'Unsure' },
+    ],
+    registrationQ13: [
       { label: 'CHAS Orange', value: 'CHAS Orange' },
       { label: 'CHAS Green', value: 'CHAS Green' },
       { label: 'CHAS Blue', value: 'CHAS blue' },
       { label: 'No CHAS', value: 'No CHAS' },
     ],
-    registrationQ9: [
+    registrationQ14: [
       { label: 'Pioneer generation card holder', value: 'Pioneer generation card holder' },
       { label: 'Merdeka generation card holder', value: 'Merdeka generation card holder' },
       { label: 'None', value: 'None' },
     ],
-    registrationQ11: [
+    registrationQ15: [
       { label: 'English', value: 'English' },
       { label: 'Mandarin', value: 'Mandarin' },
       { label: 'Malay', value: 'Malay' },
@@ -145,58 +190,58 @@ const RegForm = () => {
       <h2>Registration</h2>
       <h3>Salutation 称谓</h3>
       <SelectField name='registrationQ1' options={formOptions.registrationQ1} />
+      <h3>Initials (E.g. Alan Simon Lee as S.L Alan)</h3>
+      <LongTextField name='registrationQ3' />
+      <h3>Birthday</h3>
+      <DateField name='registrationQ4' type='date' />
+      <h3>Age</h3>
+      <GetAge />
+      <h3>Gender</h3>
+      <RadioField name='registrationQ6' options={formOptions.registrationQ6} />
       <h3>Race 种族</h3>
-      <RadioField name='registrationQ11' options={formOptions.registrationQ11} />
-      <LongTextField name='registrationQ14' />
+      <RadioField name='registrationQ7' options={formOptions.registrationQ7} />
+      <LongTextField name='registrationQ20' />
       <h3>Nationality 国籍</h3>
       <p className='paragraph--title'>
         Please Note: Non Singapore Citizens/ Non-PRs are unfortunately not eligible for this health
         screening
       </p>
-      <RadioField name='registrationQ3' options={formOptions.registrationQ3} />
+      <RadioField name='registrationQ8' options={formOptions.registrationQ8} />
       <h3>Marital Status 婚姻状况</h3>
-      <SelectField name='registrationQ4' options={formOptions.registrationQ4} />
+      <SelectField name='registrationQ9' options={formOptions.registrationQ9} />
       <h3>Occupation 工作</h3>
-      <TextField name='registrationQ5' />
+      <TextField name='registrationQ10' />
       <h3>
         GRC/SMC Subdivision{' '}
         <a href='https://www.parliament.gov.sg/mps/find-my-mp' target='_blank' rel='noreferrer'>
           [https://www.parliament.gov.sg/mps/find-my-mp]
         </a>
       </h3>
-      <SelectField name='registrationQ6' options={formOptions.registrationQ6} />
+      <SelectField name='registrationQ11' options={formOptions.registrationQ11} />
+      <h3>Are you currently part of HealthierSG?</h3>
+      <RadioField name='registrationQ12' options={formOptions.registrationQ12} />
       <h3>CHAS Status 社保援助计划</h3>
-      <SelectField name='registrationQ8' options={formOptions.registrationQ8} />
-      <h2>Follow up at GP Clinics</h2>
-      <p>
-        Your Health Report & Blood Test Results (if applicable) will be mailed out to the GP you
-        have selected <b>4-6 weeks</b> after the screening.
-      </p>
-      <h4 className='paragraph--title'>
-        All results, included those that are normal, have to be collected from the GP clinic via an
-        appointment
-      </h4>
-      {displayVacancy}
-      <RadioField name='registrationQ10' options={displayLocations()} />
+      <SelectField name='registrationQ13' options={formOptions.registrationQ13} />
       <h3> Pioneer Generation Status 建国一代配套</h3>
-      <RadioField name='registrationQ9' options={formOptions.registrationQ9} />
+      <RadioField name='registrationQ14' options={formOptions.registrationQ14} />
       <h3>Preferred Language for Health Report</h3>
-      <RadioField name='registrationQ11' options={formOptions.registrationQ11} />
-      <h2>Phlebotomy Eligibility</h2>
-      <p className='paragraph--title'>
+      <RadioField name='registrationQ15' options={formOptions.registrationQ15} />
+      <h2>Phlebotomy Eligibility</h2>{' '}
+      <p>
+        {' '}
         Before entering our screening, do note the following eligibility criteria for Phlebotomy:
         <ol type='A'>
           <li>NOT previously diagnosed with Diabetes/ High Cholesterol/ High Blood Pressure.</li>
           <li>Have not done a blood test within the past 3 years.</li>
         </ol>
       </p>
-      <p className='paragraph--title'>
+      <p>
         Rationale: PHS aims to reach out to undiagnosed people. Patients that are already aware of
         their condition would have regular follow-ups with the GPs/polyclinics/hospitals. This
         information is available in our publicity material. Please approach our registration
         volunteers should you have any queries. We are happy to explain further. Thank you!
       </p>
-      <p className='paragraph--title'>
+      <p>
         抽血合格标准:
         <br />
         1) 在过去的三年内沒有验过血。
@@ -223,6 +268,18 @@ const RegForm = () => {
         used by these parties for the purposes stated.
       </p>
       <BoolField name='registrationQ13' />
+      <h2>Follow up at GP Clinics</h2>
+      <p>
+        Your Health Report & Blood Test Results (if applicable) will be mailed out to the GP you
+        have selected <b>4-6 weeks</b> after the screening.
+      </p>
+      <h4>
+        All results, included those that are normal, have to be collected from the GP clinic via an
+        appointment
+      </h4>
+      <br />
+      {displayVacancy}
+      <RadioField name='registrationQ10' options={displayLocations()} />
     </div>
   )
   const form_layout = layout
@@ -236,6 +293,28 @@ const RegForm = () => {
     },
     registrationQ2: {
       type: String,
+      optional: false,
+    },
+    registrationQ3: {
+      type: String,
+      optional: false,
+    },
+    registrationQ4: {
+      defaultValue: new Date(),
+      type: Date,
+      optional: false,
+    },
+    registrationQ5: {
+      type: Number,
+      optional: false,
+    },
+    registrationQ6: {
+      type: String,
+      allowedValues: ['Male', 'Female'],
+      optional: false,
+    },
+    registrationQ7: {
+      type: String,
       allowedValues: [
         'Chinese 华裔',
         'Malay 巫裔',
@@ -245,7 +324,7 @@ const RegForm = () => {
       ],
       optional: false,
     },
-    registrationQ3: {
+    registrationQ8: {
       type: String,
       allowedValues: [
         'Singapore Citizen 新加坡公民',
@@ -253,7 +332,7 @@ const RegForm = () => {
       ],
       optional: false,
     },
-    registrationQ4: {
+    registrationQ9: {
       type: String,
       allowedValues: [
         'Single 单身',
@@ -264,11 +343,11 @@ const RegForm = () => {
       ],
       optional: false,
     },
-    registrationQ5: {
+    registrationQ10: {
       type: String,
       optional: false,
     },
-    registrationQ6: {
+    registrationQ11: {
       type: String,
       allowedValues: [
         'Jurong',
@@ -281,38 +360,43 @@ const RegForm = () => {
       ],
       optional: false,
     },
-    registrationQ8: {
+    registrationQ12: {
+      type: String,
+      allowedValues: ['Yes', 'No', 'Unsure'],
+      optional: false,
+    },
+    registrationQ13: {
       type: String,
       allowedValues: ['CHAS Orange', 'CHAS Green', 'CHAS Blue', 'No CHAS'],
       optional: false,
     },
-    registrationQ9: {
+    registrationQ14: {
       type: String,
       allowedValues: ['Pioneer generation card holder', 'Merdeka generation card holder', 'None'],
       optional: false,
     },
-    registrationQ10: {
+    registrationQ19: {
       type: String,
       allowedValues: Object.values(postalCodeToLocations),
       optional: true,
     },
-    registrationQ11: {
+    registrationQ15: {
       type: String,
       allowedValues: ['English', 'Mandarin', 'Malay', 'Tamil'],
       optional: false,
     },
-    registrationQ12: {
+    registrationQ16: {
       type: Boolean,
       label:
         'I have read and acknowledged the eligibility criteria for Phlebotomy. 我知道抽血的合格标准。',
       optional: false,
     },
-    registrationQ13: {
+    registrationQ17: {
       type: Boolean,
       label: 'I agree and consent to the above.',
       optional: false,
     },
-    registrationQ14: {
+    registrationQ20: {
       type: String,
       optional: true,
     },
