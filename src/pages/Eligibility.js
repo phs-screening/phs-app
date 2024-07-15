8137a79b1a2ec0ab0cc93725556f0f985040c56b
rxchell <e0969575@u.nus.edu>
2024-07-15 16:04:14 +0800

    WCE, Osteo, and NKF eligibility

--- src/pages/Eligibility.js
+++ src/pages/Eligibility.js
@@ -3,38 +3,43 @@ import { Helmet } from 'react-helmet'
 import { Box, Container, Grid, Typography } from '@mui/material'
 import { getSavedData, getSavedPatientData } from '../services/mongoDB'
 import { FormContext } from '../api/utils.js'
+import { formatBmi } from '../api/api.js'
 import allForms from '../forms/forms.json'
 
 const Eligibility = () => {
     const { patientId, updatePatientId } = useContext(FormContext)
     const [loadingPrevData, isLoadingPrevData] = useState(true)
-    const [registration , setRegistration] = useState({}) 
+    const [reg, setReg] = useState({})
     const [pmhx, setPMHX] = useState({})
     const [social, setSocial] = useState({})
     const [patient, setPatient] = useState({})
     const [hxfamily, setHxFamily] = useState({})
+    const [triage, setTriage] = useState({})
 
     useEffect(async () => {
         const loadPastForms = async () => {
             isLoadingPrevData(true)
             const pmhxData = getSavedData(patientId, allForms.hxNssForm)
             const socialData = getSavedData(patientId, allForms.hxSocialForm)
-            const registrationData = getSavedData(patientId, allForms.registrationForm)
+            const regData = getSavedData(patientId, allForms.registrationForm)
             const patientData = getSavedPatientData(patientId, 'patients')
             const hxFamilyData = getSavedData(patientId, allForms.hxFamilyForm)
+            const triageData = getSavedData(patientId, allForms.triageForm)
             
             Promise.all([
                 pmhxData,
                 socialData, 
-                registrationData, 
+                regData, 
                 patientData,
-                hxFamilyData
+                hxFamilyData, 
+                triageData
             ]).then((result) => {
                 setPMHX(result[0])
                 setSocial(result[1])
-                setRegistration(result[2])
+                setReg(result[2])
                 setPatient(result[3])
                 setHxFamily(result[4])
+                setTriage(result[5])
                 isLoadingPrevData(false)
             })
         }
@@ -99,15 +104,49 @@ const Eligibility = () => {
                     <Box sx={{ display:"flex", alignItems:"center", width:"100%" }}>
                         <Typography variant="h6" paddingRight={5} >Women’s Cancer Education & Appointment</Typography>
                         <Typography>
-                            <p className='underlined'>Are you currently part of healthiersg: {registration && registration.registrationQ12 ? registration.registrationQ12 : '-'} </p>
-                            <p className='underlined'>CHAS status: {registration && registration.registrationQ13 ? registration.registrationQ13 : '-'} </p>
-                            <p className='underlined'>Are you currently on any other Government Financial Assistance, other than CHAS and PG (e.g. Public Assistance Scheme)?: {social && social.SOCIAL3 ? social.SOCIAL3 : '-'} </p>
-                            <p className='underlined'>Is there positive family history (AMONG FIRST DEGREE RELATIVES) for the following cancers? Please specify age of diagnosis.: {hxfamily && hxfamily.FAMILY1 ? hxfamily.FAMILY1 : '-'} </p>
+                            <p className='underlined'>CHAS Status 社保援助计划: {reg && reg.registrationQ12 ? reg.registrationQ12 : '-'} </p>
+                            <p className='underlined'>Pioneer Generation Status 建国一代配套: {reg && reg.registrationQ13 ? reg.registrationQ13 : '-'} </p>
+                            <p className='underlined'>Patient on any other Government Financial Assistance, other than CHAS and PG: {social && social.SOCIAL3 ? social.SOCIAL3 : '-'} </p>
+                            <p className='underlined'>Is there positive family history (AMONG FIRST DEGREE RELATIVES) for the following cancers? : {hxfamily && hxfamily.FAMILY1 ? hxfamily.FAMILY1 : '-'} </p>
+                            <p className='underlined'>Age of diagnosis: {hxfamily && hxfamily.FAMILYShortAns1 ? hxfamily.FAMILYShortAns1 : '-'} </p>
                         </Typography>
                     </Box>
                 </Grid>
                 </>
             )}
+
+            {patient.gender === 'Female' && (
+                <>
+                <Grid item lg={8} md={8} xs={12}>
+                    <Box sx={{ display:"flex", alignItems:"center", width:"100%" }}>
+                        <Typography variant="h6" paddingRight={5} >Osteoporosis</Typography>
+                        <Typography>
+                            <p className='underlined'>Birthday: {reg && reg.registrationQ3 ? reg.registrationQ3 : '-'} </p>
+                            <p className='underlined'>Age: {reg && reg.registrationQ4 ? reg.registrationQ4 : '-'} </p>
+                            <p className='underlined'>Gender: {reg && reg.registrationQ5 ? reg.registrationQ5 : '-'} </p>
+                            <p className='underlined'>Height (in cm): {triage && triage.triageQ10 ? triage.triageQ10 : '-'} </p>
+                            <p className='underlined'>Weight (in kg): {triage && triage.triageQ11 ? triage.triageQ11 : '-'} </p>
+                            <p className='underlined'>Does patient currently smoke: {social && social.SOCIAL10 ? social.SOCIAL10 : '-'} </p>
+                            <p className='underlined'>Does patient consume alcoholic drinks: {social && social.SOCIAL12 ? social.SOCIAL12 : '-'} </p>
+                        </Typography>
+                    </Box>
+                </Grid>
+                </>
+            )}
+
+            <Grid item lg={8} md={8} xs={12}>
+                <Box sx={{ display:"flex", alignItems:"center", width:"100%" }}>
+                    <Typography variant="h6" paddingRight={5} >National Kidney Foundation</Typography>
+                    <Typography>
+                        <p className='underlined'>Age: {reg && reg.registrationQ4 ? reg.registrationQ4 : '-'} </p>
+                        <p className='underlined'>BMI: {triage && triage.triageQ9 && triage.triageQ10 ? formatBmi(triage.triageQ9, triage.triageQ10) : '-'} </p>
+                        <p className='underlined'>Patient has these conditions: {pmhx && pmhx.PMHX7 ? pmhx.PMHX7 : '-'} </p>
+                        <p className='underlined'>Patient has positive family of these conditions: {hxfamily && hxfamily.FAMILY3 ? hxfamily.FAMILY3 : '-'} </p>
+                        <p className='underlined'>Has patient has done a kidney screening in the past 1 year: {pmhx && pmhx.PMHX9 ? pmhx.PMHX9 : '-'} </p>
+                    </Typography>
+                </Box>
+            </Grid>
+            
 
             </Grid>
         </Container>
