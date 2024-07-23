167dbea0d2944516cd298158de9de249e53114ad
rxchell <e0969575@u.nus.edu>
2024-07-23 16:12:17 +0800

    update layout of eligibility page

--- src/pages/Eligibility.js
+++ src/pages/Eligibility.js
@@ -1,20 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
-import { Box, Container, Grid, Typography } from '@mui/material'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import { Box, TableContainer, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
 import { getSavedData, getSavedPatientData } from '../services/mongoDB'
 import { FormContext } from '../api/utils.js'
-import { formatBmi } from '../api/api.js'
 import allForms from '../forms/forms.json'
 
 const Eligibility = () => {
-    const { patientId, updatePatientId } = useContext(FormContext)
-    const [loadingPrevData, isLoadingPrevData] = useState(true)
+    const { patientId } = useContext(FormContext)
+    const [loadingPrevData, setLoadingPrevData] = useState(true)
     const [reg, setReg] = useState({})
     const [pmhx, setPMHX] = useState({})
     const [social, setSocial] = useState({})
@@ -23,9 +16,9 @@ const Eligibility = () => {
     const [triage, setTriage] = useState({})
     const [hcsr, setHcsr] = useState({})
 
-    useEffect(async () => {
+    useEffect(() => {
         const loadPastForms = async () => {
-            isLoadingPrevData(true)
+            setLoadingPrevData(true)
             const pmhxData = getSavedData(patientId, allForms.hxNssForm)
             const socialData = getSavedData(patientId, allForms.hxSocialForm)
             const regData = getSavedData(patientId, allForms.registrationForm)
@@ -33,7 +26,7 @@ const Eligibility = () => {
             const hxFamilyData = getSavedData(patientId, allForms.hxFamilyForm)
             const triageData = getSavedData(patientId, allForms.triageForm)
             const hcsrData = getSavedData(patientId, allForms.hxHcsrForm)
-            
+
             Promise.all([
                 pmhxData,
                 socialData, 
@@ -50,68 +43,79 @@ const Eligibility = () => {
                 setHxFamily(result[4])
                 setTriage(result[5])
                 setHcsr(result[6])
-                isLoadingPrevData(false)
+                setLoadingPrevData(false)
             })
         }
-        await loadPastForms()
-    }
-    , [patientId])
+        loadPastForms()
+    }, [patientId])
 
     function createData(name, yes, no) {
-        return { name, yes, no };
+        console.log("reg", reg);
+        return { name, yes, no }
     }
-      
+
     const rows = [
-        createData('Healthier SG Booth', 'YES', 'NO'),
-        createData('Phlebotomy', 'YES', 'NO'),
-        createData('Faecal Immunochemical Testing (FIT)','YES', 'NO'),
+        // Indicated Yes to REGI15, Checked REGI16, Checked REGI 17
+        createData('Phlebotomy', pmhx?.PMHX16 ? 'NO' : 'YES', pmhx?.PMHX16 ? 'YES' : 'NO'),
+
+        // Indicated No/Unsure to REGI11
+        createData('Healthier SG Booth', reg?.registrationQ11 != 'YES' ? 'YES' : 'NO', reg?.registrationQ11 != 'YES' ? 'NO' : 'YES'),
+        createData('Faecal Immunochemical Testing (FIT)', 'YES', 'NO'),
         createData('Lung Function Testing', 'YES', 'NO'),
-        createData('Women&apos;s Cancer Education', 'YES', 'NO'),
+        createData("Women's Cancer Education", patient.gender === 'Female' ? 'YES' : 'NO', patient.gender === 'Female' ? 'NO' : 'YES'),
         createData('Osteoporosis Screening', 'YES', 'NO'),
         createData('Kidney Screening', 'YES', 'NO'),
-        createData('Geriatric Screening','YES', 'NO'),
+        createData('Geriatric Screening', 'YES', 'NO'),
         createData('Mental Health', 'YES', 'NO'),
-        createData('Adiometry', 'YES', 'NO'),
+        createData('Audiometry', 'YES', 'NO'),
         createData('HPV On-Site Testing', 'YES', 'NO'),
-        createData('Doctor&apos;s Station','YES', 'NO'),
-        createData('Dietitian&apos;s Consult', 'YES', 'NO'),
+        createData("Doctor's Station", 'YES', 'NO'),
+        createData("Dietitian's Consult", 'YES', 'NO'),
         createData('Oral Health', 'YES', 'NO'),
         createData('Social Services', 'YES', 'NO'),
-    ];      
+    ]
+
+    const getCellStyle = (value, isAnswer) => ({
+        color: value === isAnswer ? 'blue' : 'black'
+    })
 
     return (
-    <>
-        <Helmet>
-        <title>Patient Eligibility</title>
-        </Helmet>
-        <Box sx={{
-            backgroundColor: 'background.default',
-            minHeight: '100%',
-            py: 3,
-        }} >
-        <TableContainer>
-            <Table>
-            <TableHead>
-            <TableRow>
-                <TableCell>Modality</TableCell>
-                <TableCell>ELIGIBLE?</TableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            {rows.map((row) => (
-                <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                    {row.name}
-                </TableCell>
-                <TableCell >{row.yes}</TableCell>
-                <TableCell >{row.no}</TableCell>
-                </TableRow>
-            ))}
-            </TableBody>
-        </Table>
-        </TableContainer>
-        </Box>
-    </>
+        <>
+            <Helmet>
+                <title>Patient Eligibility</title>
+            </Helmet>
+            <Box sx={{
+                backgroundColor: 'background.default',
+                minHeight: '100%',
+                py: 3,
+            }}>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Modality</TableCell>
+                                <TableCell>ELIGIBILITY (highlighted in blue)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell sx={getCellStyle(row.yes, row.yes)}>
+                                        {row.yes}
+                                    </TableCell>
+                                    <TableCell sx={getCellStyle(row.no, row.yes)}>
+                                        {row.no}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+        </>
     )
 }
 
